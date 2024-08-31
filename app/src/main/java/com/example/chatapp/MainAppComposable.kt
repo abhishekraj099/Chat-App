c5d5f47d2377d087f6a45c32809c6aa7762943b1
Abhishek raj <abhisraj099@gmail.com>
2024-09-01 01:41:58 +0530

    update4

--- app/src/main/java/com/example/chatapp/MainAppComposable.kt
+++ app/src/main/java/com/example/chatapp/MainAppComposable.kt
@@ -4,35 +4,46 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.chatapp.feature.auth.signin.LoginScreen
 import com.example.chatapp.feature.auth.signup.SignUpScreen
+import com.example.chatapp.feature.chat.ChatScreen
 import com.example.chatapp.feature.home.HomeScreen
 import com.google.firebase.auth.FirebaseAuth
 
 @Composable
 fun MainApp() {
-   Surface(modifier = Modifier.fillMaxSize()) {
-      val navController = rememberNavController()
-    val currentUser =   FirebaseAuth.getInstance().currentUser
-    val start=   if (currentUser != null) "home" else "login"
+    Surface(modifier = Modifier.fillMaxSize()) {
+        val navController = rememberNavController()
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val start = if (currentUser != null) "home" else "login"
 
 
-       NavHost(navController = navController, startDestination = start) {
+        NavHost(navController = navController, startDestination = start) {
 
 
-           composable("login") {
-               LoginScreen(navController = navController)
-           }
-           composable("signup") {
-               SignUpScreen(navController = navController)
-           }
-           composable("home") {
-               HomeScreen(navController = navController)
-           }
-           
-       }
-   }
+            composable("login") {
+                LoginScreen(navController = navController)
+            }
+            composable("signup") {
+                SignUpScreen(navController = navController)
+            }
+            composable("home") {
+                HomeScreen(navController = navController)
+            }
+            composable("chat/{channelId}", arguments = listOf(
+                navArgument("channelId") {
+                    type = NavType.StringType
+                }
+            )) {
+                val channelId = it.arguments?.getString("channelId") ?: ""
+                ChatScreen(navController = navController, channelId)
+            }
+
+        }
+    }
 }
